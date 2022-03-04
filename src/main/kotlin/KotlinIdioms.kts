c14ea6c73fea3c2775a19bbaf90ee5d62a3d1beb
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-03-04 16:58:27 +0800

    Kotlin idioms practice.

--- src/main/kotlin/KotlinIdioms.kts
+++ src/main/kotlin/KotlinIdioms.kts
@@ -3,3 +3,14 @@
 data class Rect(val width: Int, val height: Int)
 val rect = Rect(400, 400)
 println(rect)
+
+val list = listOf(-1, 2, 9, 0, 1, 2, 3, 4, 6)
+val positives = list.filter {it > 0}
+println(positives)
+
+if(-1 in list) println("yes")
+
+val map = mapOf(1 to 'a', 2 to 'b')
+for((k, v) in map)
+	println("$k, $v")
+
