1352abc49fca2dbd7c50a1f8896c8a18342b0dd6
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-03-25 00:00:44 +0800

    Programs reordered a bit.

--- src/main/kotlin/KotlinIdioms.kts
+++ src/main/kotlin/KotlinIdioms.kts
@@ -1,21 +1,24 @@
 import java.io.File
-// create data objects
 
-data class Rect(val width: Int, val height: Int)
-val rect = Rect(400, 400)
-println(rect)
+// Create DTOs (POJOs/POCOs)
+//data class Rect(val width: Int, val height: Int)
+//val rect = Rect(400, 400)
+//println(rect)
 
-val list = listOf(-1, 2, 9, 0, 1, 2, 3, 4, 6)
-val positives = list.filter {it > 0}
-println(positives)
+// Filter a list
+//val list = listOf(-1, 2, 9, 0, 1, 2, 3, 4, 6)
+//val positives = list.filter { it > 0 }
+//println(positives)
+// Check the presence of an element in a collection
+//if(-1 in list) println("yes")
 
-if(-1 in list) println("yes")
-
+// Traverse a map or a list of pairs
 //val map = mapOf(1 to 'a', 2 to 'b')
 //for((k, v) in map)
 //	println("$k, $v")
-//
-//
+
+
+// Iterate over a range
 //for(i in 0..100) print("$i ") // closed ranged  include 100
 //println()
 //
@@ -28,66 +31,68 @@ if(-1 in list) println("yes")
 //for(i in 10 downTo 0) print("$i ")
 //print("\n")
 
-object Singleton {
-	val myVal = 1
-}
-println("myVal: ${Singleton.myVal}")
+// Create a singleton
+//object Singleton {
+//	val myVal = 1
+//}
+//println("myVal: ${Singleton.myVal}")
 
-// Instantiate abstract class
-abstract class Animal {
-	abstract fun  move()
-	abstract fun eat()
-}
+// Instantiate an abstract class
+//abstract class Animal {
+//	abstract fun  move()
+//	abstract fun eat()
+//}
+//val animal = object: Animal() {
+//	override fun move() = println("Moving")
+//	override fun eat() = println("Eating")
+//}
+//animal.move()
+//animal.eat()
 
-val animal = object: Animal() {
-	override fun move() = println("Moving")
-	override fun eat() = println("Eating")
-}
-
-animal.move()
-animal.eat()
-
-// if not null shorthand
+// If not null shorthand
 val files = File(".").listFiles()
 println(files?.size)
 
-var nullable: String? = null
-println(nullable?.uppercase() ?: "null");
-nullable = "test"
-println(nullable?.uppercase() ?: "null");
+// If not null else shorthand
+//var nullable: String? = null
+//println(nullable?.uppercase() ?: "null");
+//nullable = "test"
+//println(nullable?.uppercase() ?: "null");
 
-var fruits = listOf("Apple", "Pear", "Banana")
-println(fruits.firstOrNull());
-
-fruits = listOf();
-println(fruits.firstOrNull());
+// Get possibly absent value from collection
+//var fruits = listOf("Apple", "Pear", "Banana")
+//println(fruits.firstOrNull());
+//fruits = listOf();
+//println(fruits.firstOrNull());
 
 // Execute if not null
-var z: List<Int>? = listOf(0, 1, 2, 3, 4) 
-z?.let {
-	it.filter { n -> n % 2 == 0}
-}
+//var z: List<Int>? = listOf(0, 1, 2, 3, 4) 
+//z?.let {
+//	val evenNumbers = it.filter { n -> n % 2 == 0 }
+//	println("even numbers: $evenNumbers")
+//}
 
 // Map nullable value if not null
-fun transform(): Int? = null;
-var numbers: List<Int>? = listOf(1, 3, 5, 7)
-val evenNumber = numbers?.let { transform() } ?: 2
-println("even: $evenNumber");
+//fun transform(): Int? = null;
+//var numbers: List<Int>? = listOf(1, 3, 5, 7)
+//val evenNumber = numbers?.let { transform() } ?: 2
+//println("even: $evenNumber");
 
 
 // Return on when statement
-fun reply(args: String) =  when(args) {
-	"a" -> "b"
-	else -> "c"
-}
-println(reply("b"))
+//fun reply(args: String) =  when(args) {
+//	"a" -> "b"
+//	else -> "c"
+//}
+//println(reply("b"))
 
-val trycatch = try {
-	1 / 1
-} catch(e: ArithmeticException) {
-	e.printStackTrace()
-}
-println(trycatch);
+// Try catch expression
+//val trycatch = try {
+//	1 / 1
+//} catch(e: ArithmeticException) {
+//	e.printStackTrace()
+//}
+//println(trycatch);
 
 // Call multiple methods on an object instance
 class Person {
